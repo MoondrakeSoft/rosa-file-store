b834852e692eaba3f6cace773c0c47511e1b106f
Alexander Khryukin <alexander@mezon.ru>
2015-11-16 01:52:19 +0300

    drop section with pids

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -33,31 +33,6 @@ stdout_path File.join(base_path, 'log', 'unicorn.stdout.log')
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
 
-before_fork do |server, worker|
-  # This option works in together with preload_app true setting
-  # What is does is prevent the master process from holding
-  # the database connection
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
-  
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
-
 after_fork do |server, worker|
   # Here we are establishing the connection after forking worker processes
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
