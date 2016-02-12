516467199848578b179e3c5636c5b75beca4af0c
Alexander Khryukin <alexander@mezon.ru>
2016-02-12 21:31:26 +0300

    move uploads fir from ~ to /

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -18,7 +18,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{get_last_dir_part}"
+    "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{get_last_dir_part}"
   end
 
   ## define how to partition directory
