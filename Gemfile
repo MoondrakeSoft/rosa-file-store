c7b6c23437265c2f5cf8645647e1639fd3927cf7
Alexander Khryukin <alexander@mezon.ru>
2016-05-10 02:39:06 +0300

    remove memcached and dalli

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'app'
 gem 'carrierwave'
-gem 'dalli'
 gem 'kaminari'
 gem 'puma'
 gem 'rails',                          '4.2.5'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,24 +56,24 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store,
-                       (ENV['MEMCACHIER_SERVERS'] || '').split(','),
-                       {
-                         username:             ENV['MEMCACHIER_USERNAME'],
-                         password:             ENV['MEMCACHIER_PASSWORD'],
-                         failover:             ENV['DALLI_FAILOVER']             || true,
-                         socket_timeout:       ENV['DALLI_SOCKET_TIMEOUT']       || 1.5,
-                         socket_failure_delay: ENV['DALLI_SOCKET_FAILURE_DELAY'] || 0.2,
-                         pool_size:            ENV['DALLI_CACHE_POOL']           || 10,
-                       }
+  #config.cache_store = :dalli_store,
+  #                     (ENV['MEMCACHIER_SERVERS'] || '').split(','),
+  #                     {
+  #                       username:             ENV['MEMCACHIER_USERNAME'],
+  #                       password:             ENV['MEMCACHIER_PASSWORD'],
+  #                       failover:             ENV['DALLI_FAILOVER']             || true,
+  #                       socket_timeout:       ENV['DALLI_SOCKET_TIMEOUT']       || 1.5,
+  #                       socket_failure_delay: ENV['DALLI_SOCKET_FAILURE_DELAY'] || 0.2,
+  #                       pool_size:            ENV['DALLI_CACHE_POOL']           || 10,
+  #                     }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Cache compiled assets to speed up deploys.
-  config.assets.cache_store = :dalli_store,
-                       (ENV['MEMCACHIER_SERVERS'] || '').split(','),
-                       { username: ENV['MEMCACHIER_USERNAME'], password: ENV['MEMCACHIER_PASSWORD'] }
+  # config.assets.cache_store = :dalli_store,
+  #                    (ENV['MEMCACHIER_SERVERS'] || '').split(','),
+  #                    { username: ENV['MEMCACHIER_USERNAME'], password: ENV['MEMCACHIER_PASSWORD'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
